76354e247f4aa0800f004281a7f0ffe0a9e75c49
asad <asadjawaid99@hotmail.com>
2022-08-08 19:10:38 -0400

    Mobile design

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-useless-constructor */
 import React from 'react';
 import '../../styles/componentStyles.css';
@@ -8,9 +9,48 @@ class CVPreview extends React.Component {
 	}
 
 	render() {
+		// user personal information:
+		const { userPersonalInfo } = this.props;
+		const { firstName, lastName, title, description, email, phoneNumber, linkedin, github, portfolio } =
+			userPersonalInfo;
+
 		return (
 			<div className="cv-preview-container">
-				<div className="cv-preview-header"></div>
+				{/* Header */}
+				<div className="cv-preview-header">
+					<div className="header-left">
+						<h1>Title</h1>
+						<p>Name</p>
+						<p>Phone: (xxx) xxx-xxxx</p>
+						<p>Email: example@mail.com</p>
+					</div>
+					<div className="header-right">
+						<p>
+							<strong>LinkedIn</strong>:{' '}
+						</p>
+						<p>
+							<strong>GitHub</strong>:{' '}
+						</p>
+						<p>
+							<strong>Portfolio</strong>:{' '}
+						</p>
+					</div>
+				</div>
+				{/* Section */}
+				<div className="cv-preview-description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+					make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+					typesetting, remaining essentially unchanged.
+				</div>
+				{/* Experience Section */}
+				<section className="cv-preview-experience"></section>
+				{/* Education section */}
+				<section className="cv-preview-education"></section>
+				{/* Project section */}
+				<section className="cv-preview-project"></section>
+				{/* Skills section */}
+				<section className="cv-preview-skill"></section>
 			</div>
 		);
 	}
