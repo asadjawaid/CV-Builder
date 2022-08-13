8516df6f051a386e869cabbb8cd5b114b428d6e8
asad <asadjawaid99@hotmail.com>
2022-08-13 18:19:13 -0400

    Prevent CV preview div from expanding

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -84,10 +84,16 @@ class CVPreview extends React.Component {
 				</div>
 				{/* Section */}
 				<div className="cv-preview-description">
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-					standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-					make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-					typesetting, remaining essentially unchanged.
+					{!description ? (
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+							industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+							scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+							electronic typesetting, remaining essentially unchanged.
+						</p>
+					) : (
+						<p>{description}</p>
+					)}
 				</div>
 				{/* Experience Section */}
 				<section className="cv-preview-experience"></section>
