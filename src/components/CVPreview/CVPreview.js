fa4d2adc92a3bbc933db2ebe7b8b492a5bbcab55
asad <asadjawaid99@hotmail.com>
2022-09-02 20:07:49 -0400

    Removed description section for resume

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -79,19 +79,6 @@ class CVPreview extends React.Component {
 						)}
 					</div>
 				</div>
-				{/* Section */}
-				<div className="cv-preview-description">
-					{!description ? (
-						<p>
-							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-							industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-							scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-							electronic typesetting, remaining essentially unchanged.
-						</p>
-					) : (
-						<p>{description}</p>
-					)}
-				</div>
 
 				{/* Experience Section */}
 				<section className="cv-preview-experience">
