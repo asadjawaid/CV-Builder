9e28a16c2cbff3124385b3717a12723bc88b1fed
asad <asadjawaid99@hotmail.com>
2022-09-07 11:27:51 -0400

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Container from './components/Container';
+import Footer from './components/Footer';
 
 const App = () => {
 	return (
 		<>
 			<Header />
 			<Container nameOfClass="cv-container" />
+			<Footer />
 		</>
 	);
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+	return (
+		<footer>
+			<p>Asad Jawaid</p>
+			<p>
+				<a target="_blank" href="https://github.com/asadjawaid">
+					github
+				</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
